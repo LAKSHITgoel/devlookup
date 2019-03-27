37780a8e2d1c1a515844da583f079e43dda70f9f
LAKSHITgoel <lakshit.goel@kuliza.com>
2019-03-28 00:14:10 +0530

    change in config

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-console.log(process.env.MONGO_URI);
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
-} else {
-  module.exports = require("./keys_dev");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./keys_prod");
+// } else {
+//   module.exports = require("./keys_dev");
+// }
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI:
+    "mongodb://lakshitgoel:hello123@ds125526.mlab.com:25526/dev_connector",
+  secretOrKey: "iamsecretkey"
 };
