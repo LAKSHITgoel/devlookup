045daf27f0943a8076a87476f7168b9dd6e9ef70
LAKSHIT GOEL <glakshit0@gmail.com>
2019-07-01 00:50:41 +0530

    deleted keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,14 +2,14 @@ module.exports = {
   mongoURI:
     "mongodb://lakshitgoel:hello123@ds125526.mlab.com:25526/dev_connector",
   secretOrKey: "iamsecretkey",
-  s3ID: "AKIAROSLBJXL237U6QDL",
-  seKey: "/tRtluoo1Gp9EbExyqNT/R25C9WJx7lkb+aVAErQ",
+  s3ID: "=====",
+  seKey: "==========",
   s3ProfilePicBaseURL: "https://s3.amazonaws.com/dev-buck-102/",
-  githubClientID: "65e8d51c02a8fedb6fae",
-  githubClientSecret: "b56ac33f20f16ee4397a84bf00da8acfbd7aa37c",
-  linkedinClientID: "81pn4u1dj3v7y3",
-  linkedinClientSecret: "Pqe5WOwVqLNk4Vsy",
+  githubClientID: "========",
+  githubClientSecret: "============c",
+  linkedinClientID: "==========",
+  linkedinClientSecret: "===========",
   googleClientID:
-    "1027077084455-4dk3bj9gp7jceds2nv9lpdqtkcuqaaeb.apps.googleusercontent.com",
-  googleClientSecret: "YwVeAtdsJ8PtuX4e9vZEoyJ-"
+    "====================",
+  googleClientSecret: "================"
 };
